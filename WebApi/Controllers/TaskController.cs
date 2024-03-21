b91466076294c940b4ac628e65fbd68b105946f6
Abenezer A <abenezer.aberaa@gmail.com>
2024-03-21 19:00:23 +0300

    all

--- WebApi/Controllers/TaskController.cs
+++ WebApi/Controllers/TaskController.cs
@@ -1,43 +1,113 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Features.Task.CQRS.Commands;
+using Application.Features.Task.CQRS.Queries;
+using Application.Features.Task.DTOs;
+using Domain.Enums;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class TaskControllerController : ControllerBase
+    public class TaskController : ControllerBase
     {
-        // GET: api/<TaskControllerController>
+        private readonly IMediator _mediator;
+        public TaskController(IMediator mediatR)
+        {
+            _mediator = mediatR;
+        }
+
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<List<GetTaskListDto>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            // var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userId = 1;
+            var Query = new GetTaskListQuery()
+            {
+                UserId = userId
+            };
+
+            var Tasks = await _mediator.Send(Query);
+            return Ok(Tasks);
         }
 
-        // GET api/<TaskControllerController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<GetTaskDetailDto>> Get(int id)
         {
-            return "value";
+            // var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userId = 1;
+
+            var Query = new GetTaskDetailQuery()
+            {
+                UserId = userId,
+                Id = id
+            };
+
+            var Task = await _mediator.Send(Query);
+            return Ok(Task);
         }
 
-        // POST api/<TaskControllerController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult> Post([FromBody] CreateTaskDto TaskDto)
         {
+            // var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userId = 1;
+
+            var Command = new CreateTaskCommand()
+            {
+                TaskDto = TaskDto,
+                UserId = userId
+            };
+
+            var response = await _mediator.Send(Command);
+            return Ok(response);
         }
 
-        // PUT api/<TaskControllerController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] UpdateTaskDto TaskDto)
         {
+            // var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            // if (userId == null)
+            //     throw new BadRequestException($"missing {userId}");
+            var userId = 1;
+
+            var Command = new UpdateTaskCommand()
+            {
+                updateTaskDto = TaskDto,
+                UserId = userId
+            };
+
+            var response = await _mediator.Send(Command);
+            return NoContent();
         }
 
-        // DELETE api/<TaskControllerController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpPut("Status")]
+        public async Task<ActionResult> Put(int id, string status)
         {
+
+
+            var userId = 1;
+            // var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            // if (userId == null)
+            //     throw new BadRequestException($"missing {userId}");
+
+            var Command = new UpdateTaskStatusCommand()
+            {
+                updateTaskStatusDto = new UpdateTaskStatusDto()
+                {
+                    Id = id,
+                    Status = (Status)Enum.Parse(typeof(Status), status)
+                },
+                UserId = userId
+            };
+
+            var response = await _mediator.Send(Command);
+            return NoContent();
         }
     }
 }
